0344a4336654717586003bb496259b31b28430e8
darrell bazian <darrellbazianjr@gmail.com>
2020-11-07 09:28:11 -0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/App.css";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Mando from "./assets/Mando.jpg";
+import Mudhorn from "./assets/Mudhorn.jpg";
 
-function App() {
+const App = () => {
+  const [mandoHealth, setMandoHealth] = useState(25);
+  const [mudhornHealth, setMudhornHealth] = useState(50);
+
+  const attackHandler = () => {
+    setMudhornHealth(mudhornHealth - 10);
+  };
+
+  const defendHandler = () => {
+    setMandoHealth(mandoHealth + 10);
+  };
+
+  const flamethrowerHandler = () => {
+    setMudhornHealth(mudhornHealth - 20);
+  };
+
+  const grappleHandler = () => {
+    setMudhornHealth(mudhornHealth - 5);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="containers">
+        <div className="container">
+          <h1>The Mandalorian</h1>
+          <p>
+            Health: <span>{mandoHealth}</span>
+          </p>
+          <img src={Mando} />
+        </div>
+        <div className="center-container">
+          <h1>VS</h1>
+        </div>
+        <div className="container">
+          <h1>Mudhorn</h1>
+          <p>
+            Health: <span>{mudhornHealth}</span>
+          </p>
+          <img className="mudhorn" src={Mudhorn} />
+        </div>
+      </div>
+      <div className="buttons">
+        <Button onClick={attackHandler} variant="dark">
+          Attack
+        </Button>
+        <Button onClick={defendHandler} variant="dark">
+          Defend
+        </Button>
+        <Button onClick={flamethrowerHandler} variant="dark">
+          Flame Thrower
+        </Button>
+        <Button onClick={grappleHandler} variant="dark">
+          Grapple
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../App";
+import renderer from "react-test-renderer";
+
+test("subtract 10 from mudhorn health", () => {
+  expect(mudhornHealth.toBe(40));
+});
